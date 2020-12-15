9ec34af39cf54eaa838a4bfcb1e5c9e390794b38
xszefo <piotr.sumelka@gmail.com>
2020-12-15 21:17:54 +0100

    first commit to DB

--- first_connection.py
+++ first_connection.py
@@ -6,6 +6,35 @@ import mysql.connector
 
 cnx = mysql.connector.connect(user=user, password=password,
                               host='127.0.0.1',
-                              database='employees')
+                              database='test1')
+cursor = cnx.cursor()
+
+table = 'currencies'
+
+#command = 'CREATE TABLE currencies (name VARCHAR(10), code VARCHAR(3));'
+describe = f'DESCRIBE {table}'
+select = f'SELECT * FROM {table};'
+
+#cursor.execute(command)
+#cursor.execute(describe)
+#for row in cursor:
+#	print(row)
+cursor.execute(select)
+for i in cursor:
+	print(i)
+
+insert = f"INSERT INTO {table} VALUES ('dollar', 'USD');"
+cursor.execute(insert)
+
+insert = f"INSERT INTO {table} VALUES ('euro', 'EUR');"
+cursor.execute(insert)
+
+cursor.execute(select)
+for i in cursor:
+	print(i)
+
+cnx.commit()
+
+cursor.close()
 cnx.close()
 
